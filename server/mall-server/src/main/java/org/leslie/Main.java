bc435d74c143e96d8888cfce2f663cf35433cf26
LeslieCheung <lesliechueng1996@icloud.com>
2023-02-21 02:55:21 +0800

    init root maven project

---
+++ server/mall-server/src/main/java/org/leslie/Main.java
@@ -0,0 +1,11 @@
+package org.leslie;
+
+/**
+ * @author zhang
+ * date created in ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
